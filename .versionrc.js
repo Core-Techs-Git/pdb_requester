42484e2b0a2eda5c264d364480a164320853b549
cstephf <scompaore@adimeo.com>
2020-03-11 13:59:00 +0100

    🐛(core): CHANGE REDMINE TICKET PREFIXE (R-10147)

--- .versionrc.js
+++ .versionrc.js
@@ -12,9 +12,16 @@ const security =
   '<span style="color:#ffffff;background-color:#f39c12;display:inline-block;padding:6px 10px;border-radius:4px;box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06);font-weight:bold;">SECURITY</span>';
 const unavailable =
   '<span style="color:#ffffff;background-color:#2c3e50;display:inline-block;padding:6px 10px;border-radius:4px;box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06);font-weight:bold;">UNAVAILABLE</span>';
+const head = `<p align="center">
+  <a href="https://www.laplateforme.com/">
+    <img src="https://www.laplateforme.com/cms/i?o=%2Fsites%2Fdefault%2Ffiles%2F2017-04%2Flogo_pdb_bsl-1.jpg" alt="Pdb logo" width="200">
+  </a>
+</p>
+
+# PDB REQUESTER CHANGELOG`;
 
 module.exports = {
-  header: 'PDB REQUESTER CHANGELOG',
+  header: head,
   types: [
     {
       type: '🎬',
@@ -95,4 +102,8 @@ module.exports = {
   ],
   releaseCommitMessageFormat: '🎉: RELEASE {{currentTag}}',
   issueUrlFormat: 'https://redmine.laplateforme.com/redmine/issues/{{id}}',
+  issuePrefixes: ['R-'],
+  skip: {
+    tag: true,
+  },
 };

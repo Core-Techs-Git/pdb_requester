36c240864d0bfac6e48f2e08188a0bebf0a1088c
cstephf <scompaore@adimeo.com>
2020-08-17 23:59:38 +0200

    🔗: UPDATE DEPENDENCIES TO FIX SECURITY ISSUE

--- dist/const/params.js
+++ dist/const/params.js
@@ -4,6 +4,7 @@
  * @packageDocumentation
  */
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.PARAMS = void 0;
 /** Used to ease module parameters injection. */
 exports.PARAMS = {
     SERVICE_NAME: 'SERVICE_NAME',
--- dist/const/types.js
+++ dist/const/types.js
@@ -4,6 +4,7 @@
  * @packageDocumentation
  */
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.TYPES = void 0;
 /** Used to identify which object type should be injected. */
 exports.TYPES = {
     RequesterInterface: Symbol.for('RequesterInterface'),
